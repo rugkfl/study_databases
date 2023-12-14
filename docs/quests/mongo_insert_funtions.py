9e995a597d9b52161e169da404339bfc22060bfe
rugkfl <kimgyoungha61@gmail.com>
2023-12-14 11:08:15 +0900

    수정

--- docs/quests/mongo_insert_funtions.py
+++ docs/quests/mongo_insert_funtions.py
@@ -1,11 +1,11 @@
 
 
 
-def mongo_connect() :           # mongodb에 접속하기 위한 function
+def mongo_connect(address, local, data) :           # mongodb에 접속하기 위한 function
     from pymongo import MongoClient   
-    mongoClient = MongoClient("mongodb://localhost:27017")     
-    database = mongoClient["local"]     
-    collection = database['fruits']         
+    mongoClient = MongoClient(address)     
+    database = mongoClient[local]     
+    collection = database[data]         
     return collection          # database['fruits']에 데이터를 넣어야 하므로 collection 변수를 return
 
 # 데이터
@@ -20,7 +20,12 @@ def mongo_insert(collection,add_fruits):   # insert 작업 진행을 위한 func
     for i in range(len(add_fruits)) :      # 데이터를 순서대로 하나씩 넣어주기 위한 for문
         collection.insert_one(add_fruits[i])
     return
-fruits_connect = mongo_connect()      # mongo_connect 함수 호출
+
+address = "mongodb://localhost:27017"
+local = "local"
+data = 'fruits'
+
+fruits_connect = mongo_connect(address, local, data)      # mongo_connect 함수 호출
 fruits_insert = mongo_insert(fruits_connect,fruits_info)      #  mongo_connect 함수 호출
 
 pass
