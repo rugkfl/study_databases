88d4aee29de801b343eb4d25f1d0e8b42b46c77a
rugkfl <kimgyoungha61@gmail.com>
2023-12-18 20:33:26 +0900

    실습

--- docs/quests/todolist_functions.py
+++ docs/quests/todolist_functions.py
@@ -21,85 +21,63 @@ def mongo_insert(collection,add_todo):   # insert 작업 진행을 위한 functi
 todo_list_connect = mongo_connect('todos_list') 
 # quiz_list_insert = mongo_insert(todo_list_connect, todo_list)    #  mongo_connect 함수 호출
 
-# participants insert
-user_name_list = [
-     {"참여자": "참여자 1"},
-     {"참여자": "참여자 2"},
-     {"참여자": "참여자 3"}
-]
-# user_list insert
 
-# user_list = mongo_insert(mongo_connect('participants'),user_name_list)
+user_name_list = []
+
+get_name_list=[]
+
+parti_todos_list = []
 
 
-# participants_todos insert
-parti_todos_list = [
-     {"todo list": "주간 보고서 작성", "Status": "완료"},
-     {"todo list": "이메일 확인 및 응답", "Status": "진행 중"},
-     {"todo list": "회의 준비", "Status": "진행 중"},
-     {"todo list": "팀 멤버와의 1:1 면담", "Status": "진행 중"}
-]
 
-# user_todos = mongo_insert(mongo_connect('participants_todos'),user_name_list)
-
+# user_todos = mongo_insert(mongo_connect('participants_todos'), parti_todos_list)
 
 # ---------------------------------------------------------------------------
 
-# 사용자와 title/status 입력에 대한 코드
-# parti = input("Input Your Name: ")
-# print("ToDo List 중 하나 선택 하세요 !")
-# col_todo_list = list(todo_list_connect.find({}, {"_id":0,"title":1}))
-# for i in range(len(col_todo_list)) :
-#     if i<4 :
-#         print("{}. {}".format(i+1, col_todo_list[i]["title"]), end=" , ")
-#     else :
-#         print("{}. {}".format(i+1, col_todo_list[i]["title"]))
-# num_title = int(input("Title 번호 : "))
-# input_status = input("Status : ")
-# str_input = input("종료 여부 : ")
-# pass
-
-# while str_input=='q' :
-#     parti = input("Input Your Name: ")
-#     print("ToDo List 중 하나 선택 하세요 !")
-#     col_todo_list = list(todo_list_connect.find({},{"_id":0,"title":1}))
-#     for i in range(len(col_todo_list)) :
-#         if i<4 :
-#             print("{}. {}".format(i+1, col_todo_list[i]["title"]), end=" , ")
-#         else :
-#             print("{}. {}".format(i+1, col_todo_list[i]["title"]))
-#     num_title = int(input("Title 번호 : "))
-#     input_status = input("Status : ")
-#     str_input = input("종료 여부 : ")
-
-# while str_input=='c' :
-#     for i in range(len(col_todo_list)) :
-#         if i<4 :
-#             print("{}. {}".format(i+1, col_todo_list[i]["title"]), end=" , ")
-#         else :
-#             print("{}. {}".format(i+1, col_todo_list[i]["title"]))
-#     num_title = int(input("Title 번호 : "))
-#     input_status = input("Status : ")
-#     str_input = input("종료 여부 : ")
-
-# while str_input=='x' :
-#     print("------------------")
-#     print("프로그램이 종료되었습니다.")
-#     break
    
+while True:  # 전체 loop
+    parti = input("Input Your Name: ") # 참여자 입력
+    dic_parti= {}   # 참여자의 dictionary
+    dic_parti["참여자"] = parti     #db에 '참여자'라는 key 값을 생성하고 parti 값을 value로 넣음
+    user_name_list.append(dic_parti)    #user_name_list에 dic_parti안에 있는 key값과 value를 append하여 넣어줌
+    participants = mongo_connect('participants')
+    user_list = mongo_insert(participants,user_name_list)
 
-for i in range(len(user_name_list)):
-    parti = input("Input Your Name: ")
-    print("ToDo List 중 하나 선택 하세요 !")
-    while True:
-        col_todo_list = list(todo_list_connect.find({},{"_id":0,"title":1}))
+    while True :
+        col_todo_list = list(todo_list_connect.find({}))
         for i in range(len(col_todo_list)) :
-            if i<4 :
-                print("{}. {}".format(i+1, col_todo_list[i]["title"]), end=" , ")
-            else :
-                print("{}. {}".format(i+1, col_todo_list[i]["title"]))
+                if i<4 :
+                    print("{}. {}".format(i+1, col_todo_list[i]["title"]), end=" , ")
+                else :
+                    print("{}. {}".format(i+1, col_todo_list[i]["title"]))
         num_title = int(input("Title 번호 : "))
+        participants_connect = mongo_connect('participants') 
+        col_parti_list = list(participants_connect.find({}))
+
+        for i in range(len(col_parti_list)):
+             get_name_list.append(col_parti_list[i]["참여자"])
+             pass
+        
+        for i in range(len(get_name_list)):
+            if parti == get_name_list[i]:
+                somthing_id = col_parti_list[i]['_id']
+                break
+
+        # find parti id와 이름 (참여자 이름값만 정제) v
+        # 입력한 값이랑 참여자 이름값 비교해서 맞는 주소? 맞는값 v
+        # 그 _id값을 어딘가의 변수에 저장한다. v
+        # 그 변수를 dic_num에 연결한다.
         input_status = input("Status : ")
+        
+        dic_num=[{
+             '_id' : somthing_id,
+             'Title' : num_title,
+             'Status' : input_status
+        }]
+
+        participants_todos_connect = mongo_connect('participants_todos') 
+        update_todos = mongo_insert(participants_todos_connect,dic_num) #아 몰랑
+        
         while True:
             str_input = input("종료 여부 : ")
             if str_input == "c" or str_input == "q" or str_input =="x":
@@ -107,11 +85,17 @@ for i in range(len(user_name_list)):
             else:
                 pass
         if str_input == "c":
-            pass
-        else:
-            break
-    if str_input == "q":
-        pass
-    elif str_input =="x":
-        break
-
+                continue   # 'c' 입력 시 ToDo list를 다시 출력하도록 루프의 처음으로 돌아감
+        elif str_input == 'q' :
+            print("------------------")
+            break   # 'q' 입력 시 사용자 이름을 다시 입력받도록 루프를 빠져나감
+        elif str_input =="x":
+            print("------------------")
+            print("프로그램이 종료되었습니다.")
+            exit()  # 'x' 입력 시 프로그램 종료
+    if str_input == 'q': 
+         continue  # 사용자 이름을 다시 입력받도록 루프의 처음으로 돌아감
+    
+# pass: pass는 아무것도 하지 않는다는 것을 나타내는 키워드입니다. 주로 코드의 형태를 유지하거나 나중에 작성할 코드를 표시하는데 사용
+# break: break는 현재의 반복문을 즉시 종료하고, 제어 흐름을 반복문 바로 다음의 문장으로 이동시키는 키워드입니다. break는 주로 무한루프에서 특정 조건을 만족하면 루프를 빠져나가기 위해 사용
+# continue: continue는 현재의 반복을 즉시 종료하고, 제어 흐름을 반복문의 시작 부분으로 돌려보내는 키워드입니다. continue는 주로 반복문 내에서 특정 조건을 만족하는 경우 나머지 코드를 건너뛰고 다음 반복을 시작하기 위해 사용
